efaf49ca99ba835e7769388ac07bfc5d0c5366df
Junjie <youyou6093@gmail.com>
2017-10-31 20:16:01 -0400

    123

---
+++ isodipole/gold/Ca48/compute_dipole2.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy.integrate import simps
+
+fx = np.linspace(0, 20, 801)
+
+def read_data(name):
+	denv = []
+	f = open(name, 'r')
+	for i in f:
+		rhov =  float(i.split()[3])
+		denv.append(rhov)
+	return np.array(denv)
+
+
+# only take in L = 1 channel
+def compute_integral(rhov):
+	a = simps( rhov * fx ** 3, fx)
+	b = 4 * np.pi / 3
+	return a * b
+
+
+def main():
+	# rhov_list = []
+	# result = []
+	# for i in names:
+	# 	rhov_list.append(read_data(i))
+	# 	result.append(compute_integral(read_data(i)))
+	# print result
+	para = raw_input()
+	name = "density" + para + ".txt"
+	print name
+	print para,
+	print compute_integral(read_data(name))
+
+main()
--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
-for i in {1..10};
+#!/bin/bash
+
+for i in {1..5};
 do
 ./main ${20} ${20} ${-0.01 * i}
 mv density${-0.01*i}.dat ./isodipole 
