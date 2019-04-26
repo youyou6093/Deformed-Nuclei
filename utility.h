6326b46a9558e1cadb99bf6dd439d5c0e97628e0
Junjie <youyou6093@gmail.com>
2019-04-26 18:50:33 -0400

    Add the ability to compute some super heavy elements

--- utility.h
+++ utility.h
@@ -72,6 +72,20 @@ int magic(int n){
     a.push_back(-2);
     a.push_back(+1);
     a.push_back(-7);
+    a.push_back(6);
+    a.push_back(-5);
+    a.push_back(4);
+    a.push_back(-3);
+    a.push_back(2);
+    a.push_back(-1);
+    a.push_back(-8);
+    a.push_back(7);
+    a.push_back(-6);
+    a.push_back(5);
+    a.push_back(-4);
+    a.push_back(3);
+    a.push_back(-2);
+    a.push_back(1);
     int k = -1;
     for(int i=0;i<30;i++){
         n-=2*abs(a[i]);
